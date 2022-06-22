d0abdba185d92e3db58b7653bbd0f51b72018cbd
FaridFK <74176005+FaridFK@users.noreply.github.com>
2022-06-22 22:06:48 +0700

    Add files via upload

---
+++ Konversi Suhu/Form2.Designer.vb
@@ -0,0 +1,178 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.btnproses = New System.Windows.Forms.Button()
+        Me.btnhapus = New System.Windows.Forms.Button()
+        Me.btnexit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(30, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Celcius"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(173, 19)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(60, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Reamur"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(316, 19)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(77, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Fahrenheit"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label4.Location = New System.Drawing.Point(460, 19)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(49, 20)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Kelvin"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TextBox1.Location = New System.Drawing.Point(30, 55)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 29)
+        Me.TextBox1.TabIndex = 4
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TextBox2.Location = New System.Drawing.Point(173, 55)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 29)
+        Me.TextBox2.TabIndex = 5
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TextBox3.Location = New System.Drawing.Point(316, 55)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 29)
+        Me.TextBox3.TabIndex = 6
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TextBox4.Location = New System.Drawing.Point(460, 55)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 29)
+        Me.TextBox4.TabIndex = 7
+        '
+        'btnproses
+        '
+        Me.btnproses.BackColor = System.Drawing.Color.Silver
+        Me.btnproses.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnproses.Location = New System.Drawing.Point(30, 107)
+        Me.btnproses.Name = "btnproses"
+        Me.btnproses.Size = New System.Drawing.Size(75, 36)
+        Me.btnproses.TabIndex = 8
+        Me.btnproses.Text = "PROCESS"
+        Me.btnproses.UseVisualStyleBackColor = False
+        '
+        'btnhapus
+        '
+        Me.btnhapus.BackColor = System.Drawing.Color.Silver
+        Me.btnhapus.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnhapus.Location = New System.Drawing.Point(261, 107)
+        Me.btnhapus.Name = "btnhapus"
+        Me.btnhapus.Size = New System.Drawing.Size(75, 36)
+        Me.btnhapus.TabIndex = 9
+        Me.btnhapus.Text = "DELETE"
+        Me.btnhapus.UseVisualStyleBackColor = False
+        '
+        'btnexit
+        '
+        Me.btnexit.BackColor = System.Drawing.Color.Silver
+        Me.btnexit.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnexit.Location = New System.Drawing.Point(485, 107)
+        Me.btnexit.Name = "btnexit"
+        Me.btnexit.Size = New System.Drawing.Size(75, 36)
+        Me.btnexit.TabIndex = 10
+        Me.btnexit.Text = "EXIT"
+        Me.btnexit.UseVisualStyleBackColor = False
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Aqua
+        Me.ClientSize = New System.Drawing.Size(592, 166)
+        Me.Controls.Add(Me.btnexit)
+        Me.Controls.Add(Me.btnhapus)
+        Me.Controls.Add(Me.btnproses)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form2"
+        Me.Text = "Konversi Suhu"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents btnproses As Button
+    Friend WithEvents btnhapus As Button
+    Friend WithEvents btnexit As Button
+End Class
---
+++ Konversi Suhu/Form2.vb
@@ -0,0 +1,45 @@
+﻿Public Class Form2
+    Private Sub btnproses_Click(sender As Object, e As EventArgs) Handles btnproses.Click
+        Dim c As Double = Val(TextBox1.Text)
+        Dim r As Double = Val(TextBox2.Text)
+        Dim f As Double = Val(TextBox3.Text)
+        Dim k As Double = Val(TextBox4.Text)
+
+        If TextBox1.Text <> "" = True Then
+            TextBox2.Text = (4 / 5) * c
+            TextBox3.Text = (9 / 5) * c + 32
+            TextBox4.Text = c + 273
+
+        ElseIf TextBox2.Text <> "" = True Then
+            TextBox1.Text = (5 / 4) * r
+            TextBox3.Text = (9 / 4) * r + 32
+            TextBox4.Text = (5 / 4) * r + 273
+
+        ElseIf TextBox3.Text <> "" = True Then
+            TextBox1.Text = 5 / 9 * (f - 32)
+            TextBox2.Text = 4 / 9 * (f - 32)
+            TextBox4.Text = 5 / 9 * (f - 32) + 273
+
+        ElseIf TextBox4.Text <> "" = True Then
+            TextBox1.Text = k - 273
+            TextBox2.Text = 4 / 5 * (k - 273)
+            TextBox3.Text = 9 / 5 * (k - 273) + 32
+        End If
+    End Sub
+
+    Private Sub btnhapus_Click(sender As Object, e As EventArgs) Handles btnhapus.Click
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+    End Sub
+
+    Private Sub btnexit_Click(sender As Object, e As EventArgs) Handles btnexit.Click
+        Dim tutup As String
+        tutup = MessageBox.Show("Anda yakin ingin keluar ?", "Peringatan", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If tutup = MsgBoxResult.Yes Then
+            End
+        Else
+        End If
+    End Sub
+End Class
